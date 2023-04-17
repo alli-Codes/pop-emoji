2e10d306b8ac5f06e60fadf449e567eb6db00652
alli-Codes <allisonevans081@gmail.com>
2023-04-17 19:30:53 +0100

    Creating a splitChatTexts function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const chatText = "Hello, my name is Evans #smile";
+const chatText = "Hello, my name is #Evans #smile";
+
+const splitChatTexts = function (data) {
+  return data.split(" ");
+};
 
 const findIndexOfHash = function (text) {
   text.includes("#") ? console.log(text.indexOf("#")) : console.log(No);
