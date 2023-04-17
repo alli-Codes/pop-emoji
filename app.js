3ee750cd4f0bdd531f1f6adaa75721828702b607
alli-Codes <allisonevans081@gmail.com>
2023-04-17 19:09:53 +0100

    Running the findIndexOfHash function to find the index of hash symbol

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const chatText = "Hello, my name is Evans #smile";
 
 const findIndexOfHash = function (text) {
-  text.includes("#") ? console.log("Yes") : console.log(No);
+  text.includes("#") ? console.log(text.indexOf("#")) : console.log(No);
 };
 
 const changeText2Emoji = function () {};
