3a975cafad1306cf9ed1c40ba35877ec19cb2754
alli-Codes <allisonevans081@gmail.com>
2023-04-17 19:07:33 +0100

    Testing the findIndexOfHash function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const chatText = "Hello, my name is Evans #smile";
 
 const findIndexOfHash = function (text) {
-  text.contains("#") ? console.log("Yes") : console.log(No);
+  text.includes("#") ? console.log("Yes") : console.log(No);
 };
 
 const changeText2Emoji = function () {};
 
-findIndexOfHash();
+findIndexOfHash(chatText);
