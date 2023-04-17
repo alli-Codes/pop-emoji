298f3f1a94909cd2d347f083f4687a2dd565e421
alli-Codes <allisonevans081@gmail.com>
2023-04-17 19:05:40 +0100

    Writing function to find hash location

---
+++ app.js
@@ -0,0 +1,9 @@
+const chatText = "Hello, my name is Evans #smile";
+
+const findIndexOfHash = function (text) {
+  text.contains("#") ? console.log("Yes") : console.log(No);
+};
+
+const changeText2Emoji = function () {};
+
+findIndexOfHash();
