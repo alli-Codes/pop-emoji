3a0a9ec2a4a960d6ea56798142e161ce7730038e
alli-Codes <allisonevans081@gmail.com>
2023-04-24 04:05:59 +0100

    Experimenting

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 (() => {
   const bodyElement = document.querySelector("body");
-  const bodyElementChildren = bodyElement.children;
+  const bodyElementChildren = bodyElement.childNodes;
 
-  const checkElementIsEditable = function () {
-    for (let element of bodyElementChildren) {
-      console.log(element.contentEditable);
-    }
+  const checkElementIsEditable = function (element) {
+    if (element.target.isContentEditable) console.log(true);
+    // console.log("Hey");
   };
+
+  bodyElement.addEventListener("click", (e) => checkElementIsEditable(e));
 })();
