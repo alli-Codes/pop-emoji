a30e5c25cfb23575c229582e04aac31355eac850
alli-Codes <allisonevans081@gmail.com>
2023-04-25 02:40:17 +0100

    Added the emojiMenuContainer element

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 (() => {
-  const bodyElement = document.querySelector("html");
+  const htmlElement = document.querySelector("html");
   const iconContainer = document.createElement("div");
   const icon = document.createElement("div");
+  const emojiMenuContainer = document.createElement("div");
 
   const styleCreatedElements = function (element) {
     iconContainer.style.height = `${element.clientHeight}px`;
     iconContainer.style.width = `${element.clientWidth}px`;
-    // iconContainer.style.backgroundColor = "green";
+    iconContainer.style.backgroundColor = "transparent";
     iconContainer.style.display = "flex";
     iconContainer.style.justifyContent = "end";
     iconContainer.style.position = "absolute";
     iconContainer.style.top = `${element.offsetTop}px`;
     iconContainer.style.left = `${element.offsetLeft}px`;
+    iconContainer.style.pointerEvents = "none";
 
     icon.style.height = "1rem";
     icon.style.width = "1rem";
@@ -19,18 +21,26 @@
     icon.style.transform = "translate(-.5rem, -.5rem)";
     icon.style.alignSelf = "end";
     icon.style.cursor = "pointer";
+
+    emojiMenuContainer.style.height = "10rem";
+    emojiMenuContainer.style.width = "20rem";
+    emojiMenuContainer.style.backgroundColor = "black";
+    emojiMenuContainer.style.position = "absolute";
+    emojiMenuContainer.style.transform = `translateX(-50%)`;
+    emojiMenuContainer.style.left = `${element.clientWidth / 2}px`;
+    // emojiMenuContainer.style.right = `${element.clientWidth / 2}px`;
   };
 
   const checkElementIsEditable = function (element) {
     if (element.isContentEditable) {
       styleCreatedElements(element);
       iconContainer.append(icon);
-      bodyElement.append(iconContainer);
+      htmlElement.append(iconContainer, emojiMenuContainer);
     }
   };
 
-  bodyElement.addEventListener("click", (e) => {
+  htmlElement.addEventListener("click", (e) => {
     checkElementIsEditable(e.target);
-    console.log(e);
+    // console.log(e);
   });
 })();
